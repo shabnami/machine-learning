73b5a2d062a7d57ff071cab41ed5cad3b3fa26a2
Shabnam <shabbnam.attaran@gmail.com>
2017-12-20 20:50:02 -0800

    compatible to python 3

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -174,7 +174,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env)
+    sim = Simulator(env,display = True)
     
     ##############
     # Run the simulator
